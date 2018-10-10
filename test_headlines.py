37e42adb0a9e61adeb24270632c2606d78442698
nuwagalb <isnuwa@gmail.com>
2018-10-10 16:11:59 +0300

    fix failing build #7

--- test_headlines.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from headlines import SampleTest
-
-class SampleTestTestCase(unittest.TestCase):
-    def setUp(self):
-        self.sample = SampleTest()
-
-    def test_add_method_returns_value_of_adding_two_methods(self):
-        self.assertEqual(self.sample.add(2, 3), 5)
