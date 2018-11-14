931895c7eab1bdb8e67803b081b18d0c741c8dac
nuwagalb <isnuwa@gmail.com>
2018-11-14 11:48:46 +0300

    remove unnecessary test_headlines file

--- test_headlines.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from headlines import add
-
-class HeadlinesTestCase(unittest.TestCase):
-    """class for testing methods used in the headlines module"""
-
-    def test_add_method(self):
-        """test for adding two numbers"""
-        self.assertEqual(add(2, 3), 5)
